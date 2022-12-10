43f7907137df30757ab79e784e93554fa3877ca7
Yjn024 <jiening.yu@outlook.com>
2022-12-10 10:37:53 +0800

    fix mixin

--- src/main/java/willow/train/kuayue/mixin/TrackPlacementMixin.java
+++ src/main/java/willow/train/kuayue/mixin/TrackPlacementMixin.java
@@ -1,15 +1,17 @@
 package willow.train.kuayue.mixin;
 
-import com.simibubi.create.content.logistics.trains.track.TrackPlacement;
 import org.jetbrains.annotations.Contract;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import com.simibubi.create.content.logistics.trains.track.TrackPlacement;
+
 @Mixin(TrackPlacement.class)
 public class TrackPlacementMixin {
-        @Contract(pure=true)
-        @ModifyConstant(method = "tryConnect",constant = @Constant(doubleValue = 1024.0),remap = false)
-        private static double redirectGetSquaredDistance(double constant){
-            return 16384.0;
-        }
+    @Contract(pure = true)
+    @ModifyConstant(method = "tryConnect", constant = @Constant(intValue = 32), remap = false)
+    private static int redirectGetSquaredDistance(int constant) {
+        return 1024;
+    }
 }
