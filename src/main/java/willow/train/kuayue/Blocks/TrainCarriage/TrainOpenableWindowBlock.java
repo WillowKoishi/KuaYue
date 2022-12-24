1d368e28b5bdc0f01e8fbf89c47442552c6e095d
柳树怪 <wolliw@163.com>
2022-12-25 02:25:37 +0800

    添加3种新的车窗

---
+++ src/main/java/willow/train/kuayue/Blocks/TrainCarriage/TrainOpenableWindowBlock.java
@@ -0,0 +1,158 @@
+package willow.train.kuayue.Blocks.TrainCarriage;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nullable;
+
+public class TrainOpenableWindowBlock extends TrapDoorBlock {
+    public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
+    public static final EnumProperty<Half> HALF = BlockStateProperties.HALF;
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+
+    public TrainOpenableWindowBlock(Properties pProperties) {
+        super(pProperties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, Boolean.valueOf(false)).setValue(HALF, Half.BOTTOM).setValue(POWERED, Boolean.valueOf(false)).setValue(WATERLOGGED, Boolean.valueOf(false)));
+
+    }
+
+    protected static final VoxelShape OFF_AABB = Block.box(0, 0, 0, 16, 16, 16);
+    protected static final float AABB_OFFSET = 3.0F;
+    protected static final VoxelShape NORTH_AABB = Block.box(0, 0, 15, 16, 16, 17);
+    protected static final VoxelShape  WEST_AABB= Block.box(15, 0, 0, 17, 16, 16);
+    protected static final VoxelShape SOUTH_AABB = Block.box(0, 0, -1, 16, 16, 1);
+    protected static final VoxelShape EAST_AABB = Block.box(-1, 0, 0, 1, 16, 16);
+
+//    private boolean canAttachTo(BlockGetter pBlockReader, BlockPos pPos, Direction pDirection) {
+//        BlockState blockstate = pBlockReader.getBlockState(pPos);
+//        return blockstate.isFaceSturdy(pBlockReader, pPos, pDirection);
+//    }
+
+
+//    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+//        return true;
+//    }
+
+    public VoxelShape getShape(BlockState p_54372_, BlockGetter p_54373_, BlockPos p_54374_, CollisionContext p_54375_) {
+
+
+        switch ((Direction) p_54372_.getValue(FACING)) {
+            case NORTH:
+                return NORTH_AABB;
+            case SOUTH:
+                return SOUTH_AABB;
+            case WEST:
+                return WEST_AABB;
+            case EAST:
+            default:
+                return EAST_AABB;
+        }
+
+
+    }
+
+    private Half getHinge(BlockPlaceContext pContext) {
+        BlockGetter blockgetter = pContext.getLevel();
+        BlockPos blockpos = pContext.getClickedPos();
+        Direction direction = pContext.getHorizontalDirection();
+        BlockPos blockpos1 = blockpos.above();
+        Direction direction1 = direction.getCounterClockWise();
+        BlockPos blockpos2 = blockpos.relative(direction1);
+        BlockState blockstate = blockgetter.getBlockState(blockpos2);
+        BlockPos blockpos3 = blockpos1.relative(direction1);
+        BlockState blockstate1 = blockgetter.getBlockState(blockpos3);
+        Direction direction2 = direction.getClockWise();
+        BlockPos blockpos4 = blockpos.relative(direction2);
+        BlockState blockstate2 = blockgetter.getBlockState(blockpos4);
+        BlockPos blockpos5 = blockpos1.relative(direction2);
+        BlockState blockstate3 = blockgetter.getBlockState(blockpos5);
+        int i = (blockstate.isCollisionShapeFullBlock(blockgetter,
+                blockpos2) ? -1 : 0) + (blockstate1.isCollisionShapeFullBlock(blockgetter, blockpos3) ? -1 : 0)
+                + (blockstate2.isCollisionShapeFullBlock(blockgetter, blockpos4) ? 1 : 0)
+                + (blockstate3.isCollisionShapeFullBlock(blockgetter, blockpos5) ? 1 : 0);
+        //boolean flag = blockstate.is(this) && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER;
+        //boolean flag1 = blockstate2.is(this) && blockstate2.getValue(HALF) == DoubleBlockHalf.LOWER;
+        if (i <= 0) {
+            if (i >= 0) {
+                int j = direction.getStepX();
+                int k = direction.getStepZ();
+                Vec3 vec3 = pContext.getClickLocation();
+                double d0 = vec3.x - (double) blockpos.getX();
+                double d1 = vec3.z - (double) blockpos.getZ();
+                return (j >= 0 || !(d1 < 0.5D)) && (j <= 0 || !(d1 > 0.5D)) && (k >= 0 || !(d0 > 0.5D)) && (k <= 0 || !(d0 < 0.5D)) ? Half.BOTTOM : Half.TOP;
+            } else {
+                return Half.BOTTOM;
+            }
+        } else {
+            return Half.TOP;
+        }
+    }
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        FluidState fluidstate = pContext.getLevel().getFluidState(pContext.getClickedPos());
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite())
+                .setValue(HALF, this.getHinge(pContext)).setValue(OPEN, false).setValue(POWERED, false)
+                .setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
+    }
+
+
+    @Override
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
+        //pLevel.setBlock(pPos.above(), pState.setValue(HALF, DoubleBlockHalf.UPPER), 3);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+
+        pState = pState.cycle(OPEN);
+        pLevel.setBlock(pPos, pState, 10);
+        //pLevel.levelEvent(pPlayer, pState.getValue(OPEN) ? this.getOpenSound() : this.getCloseSound(), pPos, 0);
+        pLevel.gameEvent(pPlayer, this.isOpen(pState) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pPos);
+        return InteractionResult.sidedSuccess(pLevel.isClientSide);
+
+    }
+
+    public boolean isOpen(BlockState pState) {
+        return pState.getValue(OPEN);
+    }
+
+    public void setOpen(@Nullable Entity p_153166_, Level pLevel, BlockState pState, BlockPos pPos, boolean pOpen) {
+        if (pState.is(this) && pState.getValue(OPEN) != pOpen) {
+            pLevel.setBlock(pPos, pState.setValue(OPEN, Boolean.valueOf(pOpen)), 10);
+            //this.playSound(pLevel, pPos, pOpen);
+            pLevel.gameEvent(p_153166_, pOpen ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pPos);
+        }
+    }
+
+
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+}
